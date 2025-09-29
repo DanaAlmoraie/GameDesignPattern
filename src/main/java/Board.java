99b1cc42fac8d7f3843baac9cbf50558c77c5783
Joudj7tam <joodtalbz@gmail.com>
2025-09-29 23:23:43 +0300

    Applying Facory And Prototype Creational Design Patterns

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -3,7 +3,7 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
-import java.awt.Image;
+ import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
@@ -14,7 +14,6 @@ import java.util.Random;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
-import javax.imageio.ImageIO;
 
 /**
  * 
